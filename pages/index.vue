1a45329cd1ca17d7494db4694de2f21e1b7f494c
soorajkavumpadi <soorajkavumpadi@gmail.com>
2022-03-02 17:12:10 +0530

    Update index.vue
    test 23

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,6 @@
 <template>
   <div id="home-page" class="page-wrapper home-page">
-    <site-hero :title="title" :subtitle="subtitle" :image="featureImage">
-      <button
-        v-if="$siteConfig.newsletter.on"
-        class="button is-primary"
-        @click="$eventBus.$emit('modal-triggered', 'newsletter-modal')"
-      >
-        Subscribe To News
-      </button>
-    </site-hero>
+    
     <main-section theme="one-column">
       <template v-slot:default>
         <!-- All Posts -->
